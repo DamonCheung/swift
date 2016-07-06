16471edeae3d8e4bfa16b7f2654d9b6e5160a078
damon_yun <276062421@qq.com>
2016-07-06 09:57:40 +0000

    Modified

--- methods.swift
+++ methods.swift
@@ -1,3 +1,10 @@
+
+//Swift 默认仅给方法的第一个参数名称一个局部参数名称；默认同时给第二个和后后续的参数
+//名称为全局参数名称
+
+//是否提供外部名称设置
+//我们强制在第一个参数添加外部名称把这个局部名称当作外部名称使用（Swift 2.0前是使用 # 号）。
+//相反，我们呢也可以使用下划线（_）设置第二个及后续的参数不提供一个外部名称。
 //import Cocoa
 
 class calculations {
@@ -18,10 +25,9 @@ class calculations {
     }
 }
 
-let pri = calculations(a:10,b:10);
+let pri = calculations(a:10, b:10);
 
 pri.show();
 
 pri.add(10);
-pri.add(c:10);
 pri.show();
--- properties.swift
+++ properties.swift
@@ -14,3 +14,110 @@ n.digits = 67;
 print(n.digits);
 print(n.pi);
 
+//延迟存储属性
+//import Cocoa
+
+class sample{
+    lazy var no = number();
+}
+
+class number{
+    var name = "Damon";
+}
+
+var firstsample = sample();
+print(firstsample.no.name);
+
+//计算属性
+
+//import Cocoa
+
+class sample1{
+    var no1 = 0.0,no2 = 0.0;
+    var length = 300.0, breadth = 150.0;
+
+    var middle:(Double, Double){
+        get{
+            return (length/2,breadth/2);
+        }
+        set(arg){
+            no1 = arg.0 - length;
+            no2 = arg.1 - breadth;
+        }
+    }
+}
+
+var result = sample1();
+print(result.middle);
+result.middle = (0.0,10.0);
+
+print(result.no1);
+print(result.no2);
+
+//import Cocoa
+
+class film {
+    var head = ""
+    var duration = 0.0
+    var metaInfo: [String:String] {
+        return [
+            "head": self.head,
+            "duration":"\(self.duration)"
+        ]
+    }
+}
+
+var movie = film()
+movie.head = "Swift 属性"
+movie.duration = 3.09
+
+print(movie.metaInfo["head"]!)
+print(movie.metaInfo["duration"]!)
+
+//import Cocoa
+
+class Samplepgm {
+    var counter: Int = 0{
+        willSet(newTotal){
+            print("计数器: \(newTotal)")
+        }
+        didSet{
+            if counter > oldValue {
+                print("新增数 \(counter - oldValue)")
+            }
+        }
+    }
+}
+let NewCounter = Samplepgm()
+NewCounter.counter = 100
+NewCounter.counter = 800
+
+//import Cocoa
+
+struct StudMarks {
+   static let markCount = 97
+   static var totalCount = 0
+   var InternalMarks: Int = 0 {
+      didSet {
+         if InternalMarks > StudMarks.markCount {
+            InternalMarks = StudMarks.markCount
+         }
+         if InternalMarks > StudMarks.totalCount {
+            StudMarks.totalCount = InternalMarks
+         }
+      }
+   }
+}
+
+var stud1Mark1 = StudMarks()
+var stud1Mark2 = StudMarks()
+
+stud1Mark1.InternalMarks = 98
+print(stud1Mark1.InternalMarks) 
+
+stud1Mark2.InternalMarks = 87
+print(stud1Mark2.InternalMarks)
+
+
+
+
